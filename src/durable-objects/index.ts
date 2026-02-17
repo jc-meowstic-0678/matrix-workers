04203b81f57432677aa2f36e55a1095f43dcd4bb
JCMeowstic <261815398+jc-meowstic-0678@users.noreply.github.com>
2026-02-16 16:48:39 -0800

    Update index.ts

--- src/durable-objects/index.ts
+++ src/durable-objects/index.ts
@@ -1,7 +1,7 @@
 export { RoomDurableObject } from './RoomDurableObject';
 export { SyncDurableObject } from './SyncDurableObject';
 export { FederationDurableObject } from './FederationDurableObject';
-export { CallRoomDurableObject } from './call-room';
+export { CallRoomDurableObject } from './CallRoom';
 export { AdminDurableObject } from './AdminDurableObject';
 export { UserKeysDurableObject } from './UserKeysDurableObject';
 export { PushDurableObject } from './PushDurableObject';
